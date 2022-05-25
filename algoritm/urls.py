a8554ee32c297feef7bceb06f115cd562e852676
aget777 <9240177@gmail.com>
2022-05-25 19:24:38 +0500

    связали таблицу категорий и таблицу постов, добавили на сайт отображение вертикального меню из БД

--- algoritm/urls.py
+++ algoritm/urls.py
@@ -6,4 +6,9 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('products/', products, name='products'),
+    path('addpage/', addpage, name='addpage'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
--- algoritm/views.py
+++ algoritm/views.py
@@ -1,17 +1,63 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from .models import *
 
-menu = ['Главная', 'Услуги', 'О нас']
+menu = [{'title': 'Главная', 'url_name': 'home'},
+        {'title': 'Услуги', 'url_name': 'products'},
+        {'title': 'Добавить статью', 'url_name': 'addpage'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},]
+
 
 
 def index(request):
-
-    return render(request, 'algoritm/index.html', {'menu': menu, 'title' : 'Главная страница'})
+    context = {
+        'menu': menu,
+        'title': 'Главная страница',
+    }
+    return render(request, 'algoritm/index.html', context=context)
 
 def products(request):
     posts = Boards.objects.all()
-    return render(request, 'algoritm/products.html', {'posts': posts, 'menu': menu, 'title' : 'О сайте'})
+    cats = Category.objects.all()
+    context = {
+        'cats': cats,
+        'posts': posts,
+        'menu': menu,
+        'title': 'Услуги',
+        'cat_selected' : 0,
+    }
+    return render(request, 'algoritm/products.html', context=context)
 
 
+def show_category(request, cat_id):
+    posts = Boards.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
 
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по категориям',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'algoritm/products.html', context=context)
+
+
+def addpage(request):
+    return HttpResponse("Добавить статью")
+
+def show_post(request, post_id):
+    return HttpResponse(f"{post_id}")
+
+
+def contact(request):
+    return HttpResponse("Страница контакты")
+
+def login(request):
+    return HttpResponse("Вход в личный кабинет")
+
+def pageNotFound(request, excexption):
+    return HttpResponseNotFound(f"<h1>Такая страница не найдена</h1>")
