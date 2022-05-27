43fca20c21d0ec6313bf09425b26e8b315bce38e
aget777 <9240177@gmail.com>
2022-05-27 17:44:32 +0500

    сделаи супер юзера, настроили поля админки, добавили изображения через адмику

--- algoritm/apps.py
+++ algoritm/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AlgoritmConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'algoritm'
+    verbose_name = f'Наш сайт {name}'
