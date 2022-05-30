9da9f33c4f86e3b5dcea480b0c74abffacf71e48
aget777 <9240177@gmail.com>
2022-05-30 15:24:26 +0500

    добавили дебагер и кеширование страниц

--- algoritm/urls.py
+++ algoritm/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from .views import *
-
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
+    # path('', cache_page(60)(AlgoritmHome.as_view()), name='home'),
+    path('', AlgoritmHome.as_view(), name='home'),
     # path('', index, name='home'),
     path('', AlgoritmHome.as_view(), name='home'),
+    # path('products/', cache_page(60)(AlgoritmProducts.as_view()), name='products'),
     path('products/', AlgoritmProducts.as_view(), name='products'),
     path('addpage/', AddPage.as_view(), name='addpage'),
     path('contact/', contact, name='contact'),
--- algoritm/views.py
+++ algoritm/views.py
@@ -51,8 +51,9 @@ class AlgoritmCategory(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat), cat_selected=context['posts'][0].cat_id)
-
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        # c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat), cat_selected=context['posts'][0].cat_id)
+        c_def = self.get_user_context(title='Категория - ' + str(c.name), cat_selected=c.pk)
         return dict(list(context.items()) + list(c_def.items()))
 
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
